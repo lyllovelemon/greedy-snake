0d0ff275318f3af2e0c9099945f8387962fbe108
lyllovelemon <732193462@qq.com>
2020-02-15 20:56:03 +0800

    feat:定时器实现贪吃蛇自动移动

--- src/move.js
+++ src/move.js
@@ -1,5 +1,5 @@
 let container=document.querySelector('.container');
-console.dir(container);
+
 let eggX=Math.floor(Math.random()*10);
 let eggY=Math.floor(Math.random()*10);
 let egg=container.children[eggX].children[eggY];
@@ -8,55 +8,51 @@ let snakeX=Math.floor(Math.random()*10);
 let snakeY=Math.floor(Math.random()*10);
 let snakeArr=[{
     x:snakeX,y:snakeY
-}]
-console.log('snakeArr',snakeArr)
+}];
+
 snakeArr.forEach((item,index)=>{
-    let snake=container.children[snakeX].children[snakeY];
+    let snake=container.children[item.x].children[item.y];
     if(index===0){
         snake.innerHTML='<div class="snake snake-head"></div>';
+        clearInterval()
     }
     else {
         snake.innerHTML='<div class="snake snake-tail"></div>';
     }
-})
-
-//键盘监听
-document.addEventListener('keydown',function(e){
-    console.log('keydown------->',e);
-    let snakeHead=snakeArr[0]
-    if(e.key==='ArrowUp'){
-        let snakeArr2=[]
+});
+var arrow='ArrowRight';//默认移动方向-右
+function move() {
+    let snakeHead=snakeArr[0];
+    if(arrow==='ArrowUp'){
+        let snakeArr2=[];
         snakeArr.forEach((item,index)=>{
             if(index===0){
                 snakeArr2.push({
                     x:snakeArr[index].x-1,
                     y:snakeArr[index].y
-                })
+                });
             }
             else {
                 snakeArr2.push({
                     x:snakeArr[index-1].x,
                     y:snakeArr[index-1].y
-                })
+                });
             }
         })
-        snakeArr=snakeArr2
-        snakeHead.x-=1;
-        //蛇吃到蛋
-        if(snakeHead.x===eggX&&snakeHead.y===eggY) {
-            //旧蛋被吃，生成新蛋
+        snakeArr=snakeArr2;
+        if(snakeHead.x===eggX&&snakeHead.y===eggY){
             eggX=Math.floor(Math.random()*10);
             eggY=Math.floor(Math.random()*10);
             egg=container.children[eggX].children[eggY];
-            egg.innerHTML='<div class="egg"></div>'
-            let snakeTail=snakeArr[snakeArr.length-1]
+            egg.innerHTML='<div class="egg"></div>';
+            let snakeTail=snakeArr[snakeArr.length-1];
             snakeArr.push({
                 x:snakeTail.x-1,
                 y:snakeTail.y
             })
         }
     }
-    if(e.key==='ArrowDown'){
+    if(arrow==='ArrowDown'){
         let snakeArr2=[]
         snakeArr.forEach((item,index)=>{
             if(index===0){
@@ -87,7 +83,7 @@ document.addEventListener('keydown',function(e){
             })
         }
     }
-    if(e.key==='ArrowLeft'){
+    if(arrow==='ArrowLeft'){
         let snakeArr2=[]
         snakeArr.forEach((item,index)=>{
             if(index===0){
@@ -103,8 +99,7 @@ document.addEventListener('keydown',function(e){
                 })
             }
         })
-        snakeArr=snakeArr2
-        snakeHead.y-=1
+        snakeArr=snakeArr2;
         if(snakeHead.x===eggX&&snakeHead.y===eggY) {
             //旧蛋被吃，生成新蛋
             eggX=Math.floor(Math.random()*10);
@@ -118,7 +113,7 @@ document.addEventListener('keydown',function(e){
             })
         }
     }
-    if(e.key==='ArrowRight'){
+    if(arrow==='ArrowRight'){
         let snakeArr2=[]
         snakeArr.forEach((item,index)=>{
             if(index===0){
@@ -149,23 +144,38 @@ document.addEventListener('keydown',function(e){
             })
         }
     }
-    //删除上一个贪吃蛇
+    //删除移动前的贪吃蛇(清除贪吃蛇前一次的位置)
     container.querySelectorAll('.snake').forEach((item,index)=>{
         item.parentElement.removeChild(item);
-    })
+    });
     //生成贪吃蛇
     snakeArr.forEach((item,index)=>{
-        if(item.x<0||item.y<0){
-            alert('game over')
-            return
+        if(item.x<0||item.y<0||item.x>9||item.y>9){
+            clearInterval()
+            return;
         }
         var snake=container.children[item.x].children[item.y];
+
+        console.log('snake------>',snake)
         if(index===0){
             snake.innerHTML='<div class="snake snake-head"></div>';
+
         }
         else {
             snake.innerHTML='<div class="snake snake-tail"></div>';
         }
     })
+}
+//键盘监听
+document.addEventListener('keydown',function(e){
+    arrow=e.key;
+    console.log('arrow------>',arrow,e);
+    move();
 
-})
+});
+setInterval(function () {
+    move();
+},1000)
+
+
+
